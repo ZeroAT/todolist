6fd1b3d91adbf70df723ac8fc6cd2e3b00aec804
ZeroAT <zeroat@live.com>
2019-07-05 12:00:53 -0400

    To Do Model/View

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreToDo.Controllers
+{
+    public class ToDoController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Get to do items from db
+
+            //put items into a model
+
+            //render view using the model
+
+            return View();
+        }
+    }
+}
